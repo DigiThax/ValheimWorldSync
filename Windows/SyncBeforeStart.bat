b443e55f4e7ee1400ee2886afe8d771e509bffa6
KT <digithax@gmail.com>
2025-01-03 09:01:03 +0530

    Added some merge conflict error messages

--- Windows/SyncBeforeStart.bat
+++ Windows/SyncBeforeStart.bat
@@ -1,10 +1,15 @@
 @echo off
 echo Pulling latest world from Git
 git pull origin main
+if %errorlevel% neq 0 (
+    echo Pull failed - resolve conflicts manually.
+    pause
+    exit /b
+)
 
 echo Copying world to Valheim directory
-copy /Y C:\ValheimSync\sharedworld.db %AppData%\..\LocalLow\IronGate\Valheim\worlds_local\sharedworld.db
-copy /Y C:\ValheimSync\sharedworld.fwl %AppData%\..\LocalLow\IronGate\Valheim\worlds_local\sharedworld.fwl
+copy /Y C:\ValheimWorldSync\Falkreath.db %AppData%\..\LocalLow\IronGate\Valheim\worlds_local\Falkreath.db
+copy /Y C:\ValheimWorldSync\Falkreath.fwl %AppData%\..\LocalLow\IronGate\Valheim\worlds_local\Falkreath.fwl
 
 echo Sync complete. Ready to play
 pause
